000cb9da9928c8ff49f421ded0b164da4bf4a07f
demnagel <35008815+demnagel@users.noreply.github.com>
2018-11-15 17:05:55 +0300

    Update 1.py
    вот

--- 1.py
+++ 1.py
@@ -5,6 +5,8 @@ from scipy.spatial.distance import cosine
 text = open('text.txt', 'r', encoding='utf-8')
 
 def counter(text):
+    data = {}
+    final = {}
     result = []
     arr = []
     matr = []
@@ -14,7 +16,7 @@ def counter(text):
         arr.extend(st)
         matr.append(st)
     res_dict = {el: arr.count(el) for el in arr}
-
+    i = 1
     for str in matr:
         c_str = []
         for word, count in res_dict.items():
@@ -22,8 +24,10 @@ def counter(text):
                 c_str.append(count)
             else:
                 c_str.append(0)
-        if result[0]:
-            print(cosine(result[0]), c_str)
         result.append(c_str)
-    return result
-print(counter(text))
+        final[i] = (round(cosine(result[0], c_str),2))
+        i += 1
+        data={'word': res_dict, 'matrix':result, 'result':final}
+    return data
+
+x = counter(text)
